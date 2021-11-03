40e66a9dfaf446208ab08c159d6ebda93fa73687
pavelstarshinov@gmail.com <pav$sta2>
2021-11-03 20:08:39 +0300

    03/11

--- result_prev.py
+++ result_prev.py
@@ -1,27 +1,27 @@
-def all_moves_avail(field):   #формирвание списка всех ходов
+def all_moves_avail(FIELD):   #формирвание списка всех ходов
     zero_square = 10
     all_moves = []
-    for i in range(1, field+1):
-        for j in range(1, field+1):
+    for i in range(1, FIELD+1):
+        for j in range(1, FIELD+1):
             current_square = zero_square + j
             all_moves_new = all_moves + [current_square]
             all_moves = all_moves_new
         zero_square += 10
     return all_moves
 
-def print_field(field):   #печать поля
+def print_field(FIELD):   #печать поля
     zero_square = 10
-    print("_______" * field)
-    for i in range(1, field+1):
-        for j in range(1, field+1):
+    print("_______" * FIELD)
+    for i in range(1, FIELD+1):
+        for j in range(1, FIELD+1):
             current_square = zero_square + j
             if current_square in list_pl1:
-                current_square = "88"
+                current_square = "01"
             if current_square in list_pl2:
                 current_square = "00"
             print(" ", current_square, " ", end="|")
         zero_square += 10
-        print("\n", "______|" * field, sep="")
+        print("\n", "______|" * FIELD, sep="")
 
 def move_check_hor(move, list_pl):   #проверка комбинуии по горизонтали
     kombination_hor = 1
@@ -78,65 +78,94 @@ def move_check(move, list_pl, pl):   #проверка всех комбинац
     kombination_ver = move_check_ver(move, list_pl)
     kombination_diag_1 = move_check_diag_1(move, list_pl)
     kombination_diag_2 = move_check_diag_2(move, list_pl)
-    if kombination_hor == field:
+    if kombination_hor == FIELD:
         winner = pl
-    elif kombination_ver == field:
+    elif kombination_ver == FIELD:
         winner = pl
-    elif kombination_diag_1 == field:
+    elif kombination_diag_1 == FIELD:
         winner = pl
-    elif kombination_diag_2 == field:
+    elif kombination_diag_2 == FIELD:
         winner = pl
     return winner
 
+def get_field_size():
+    while True:
+        try:
+           FIELD = int(input("Введите размер поля"))
+        except ValueError:
+           print("Введите размер поля еще раз")
+        else:
+            return FIELD
+
+    while True:
+        FIELD = input("Введите размер поля")
+        if not str(FIELD).isdigit():
+            print("Введите размер поля еще раз")
+        else:
+            return int(FIELD)
+
+def get_plr_step(help_text: str):
+    move = int(input(help_text))
+    if not str(move).isdigit():
+        print(help_text)
+    else:
+        while move not in all_moves:  # проверка возможности хода
+            print_field(FIELD)
+            print("такая клетка занята или отсутствует")
+            move = int(input(help_text))
+        return move
+
+def win_check(move, list_pl, pl):
+    if move_check(move, list_pl, pl) == pl:
+        list_pl = list_pl + [move]
+        all_moves.remove(move)
+        print_field(FIELD)
+        print(f"Игрок {pl} выиграл!")
+    else:
+        list_pl = list_pl + [move]
+        all_moves.remove(move)
+    return move_check(move, list_pl, pl), list_pl, all_moves
+
+
 if __name__ == "__main__":
 
-    field = int(input("Введите размер поля: "))
+    FIELD = get_field_size()
+
+    #dict_list_pl = {  # todo dict, чтобы убрать списки list_pl1 и list_pl2
+       # 1: [],
+       # 2: []
+    #}
 
     list_pl1 = []
     list_pl2 = []
 
-    all_moves = all_moves_avail(field)
-    print_field(field)
+    all_moves = all_moves_avail(FIELD)
+    print_field(FIELD)
 
     while True:
 
-        move1 = int(input("Игрок 1, Введите ваш ход(выберите номер клетки, куда поставить крестик): "))
+        move = get_plr_step("Игрок 1, Введите ваш ход(выберите номер клетки, крестик - это 01): ")
+        print("ход:", move)
 
-        while move1 not in all_moves:   #проверка возможности хода
-            print_field(field)
-            print("такая клетка занята или отсутствует")
-            move1 = int(input("Повторите ваш ход: "))
-
-        move = move1
         list_pl = list_pl1
         pl = 1
-        if move_check(move, list_pl, pl) == 1:
-            list_pl1 = list_pl1 + [move1]
-            all_moves.remove(move1)
-            print_field(field)
-            print("Игрок 1 выиграл!")
+        win_check(move, list_pl1, pl)
+        if move_check(move, list_pl, pl) == pl:
             break
-
-        list_pl1 = list_pl1 + [move1]
-        all_moves.remove(move1)
+        else:
+            list_pl1 = list_pl1 + [move]
 
         if not all_moves:
-            list_pl1 = list_pl1 + [move1]
-            print_field(field)
+            print_field(FIELD)
             print("Игра окончена. Ничья!")
             break
 
-        print_field(field)
+        print_field(FIELD)
 
         move2 = int(input("Игрок 2, Введите ваш ход(выберите номер клетки, куда поставить нолик): "))
 
-        #while isinstance(move2, int) != True:
-            #all_moves_avail(field)
-            #print("Вы ввели не число, введите номер клетки")
-            #move2 = int(input("Повторите ваш ход: "))
-
         while move2 not in all_moves:
-            print_field(field)
+            print_field(FIELD)
             print("такая клетка занята или отсутствует")
             move2 = int(input("Повторите ваш ход: "))
 
@@ -146,7 +175,7 @@ if __name__ == "__main__":
         if move_check(move, list_pl, pl) == 2:
             list_pl2 = list_pl2 + [move2]
             all_moves.remove(move2)
-            print_field(field)
+            print_field(FIELD)
             print("Игрок 2 выиграл!")
             break
 
@@ -155,9 +184,9 @@ if __name__ == "__main__":
 
         if not all_moves:
             list_pl2 = list_pl2 + [move2]
-            print_field(field)
+            print_field(FIELD)
             print("Игра окончена. Ничья!")
             break
 
-        print_field(field)
+        print_field(FIELD)
 
